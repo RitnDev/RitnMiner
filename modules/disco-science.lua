a104a831905b23f16e39a7e65438bc864023a920
RitnDev <ritn@hotmail.fr>
2024-06-26 04:01:55 +0200

    0.8.6 - migration RitnLib 0.8.2

--- modules/disco-science.lua
+++ modules/disco-science.lua
@@ -1,7 +1,7 @@
-local RitnEvent = require(ritnlib.defines.class.luaClass.event)
+
 ----------------------------------------------------------------
 local function updateDiscoScience()
-    RitnEvent.setIngredientColor("miner-science-pack", {r = 0.592, g = 0.565, b = 0.808})
+    RitnLibEvent.setIngredientColor("miner-science-pack", {r = 0.592, g = 0.565, b = 0.808})
 end
 ----------------------------------------------------------------
 script.on_configuration_changed(updateDiscoScience)
--- modules/miner.lua
+++ modules/miner.lua
@@ -1,11 +1,9 @@
 local tech = require("modules.update-recipes")
 ----------------------------------------------------------------
-local RitnEvent = require(ritnlib.defines.class.luaClass.event)
-----------------------------------------------------------------
 
   -- Quand une recherche se termine...
 local function on_research_finished(e)
-    local RitnTech = RitnEvent(e):getTechnology()
+    local RitnTech = RitnLibEvent(e):getTechnology()
     local iTech = tech[RitnTech.name]
     if iTech then
         RitnTech:updateRecipe(iTech.name, iTech.disableTabRecipes, iTech.setRecipeName)
